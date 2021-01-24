3986679fb82789ffd33acf2287d16f47afcbf8ec
CCnova <caiocnova1@gmail.com>
2021-01-24 14:53:24 -0300

    chore(): Add postgre and pg library for npm

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+
+async function test() {
+  const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
+  });
+  const res = await pool.query('SELECT NOW()');
+  console.log(res);
+}
+
+test();
+
 const app = require('express')();
 
 app.use(require('body-parser').json());
